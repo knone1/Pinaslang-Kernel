472079021916789f0880955c7fa0cf752700c7c7
knone1 <knone.null@gmail.com>
2014-12-09 10:08:26 +0800

    uploading

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 11 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#11 PREEMPT Tue Dec 9 08:38:37 PHT 2014"
+#define LINUX_COMPILE_TIME "08:38:37"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "PinasLang"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
